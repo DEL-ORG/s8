342677b8d90436d609926cddda6733009fd93a9e
danniella97 <123521893+danniella97@users.noreply.github.com>
2025-01-03 01:30:21 -0500

    Update main.tf

--- terraform/resources/dev/aws-secret-manager/main.tf
+++ terraform/resources/dev/aws-secret-manager/main.tf
@@ -1,10 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-## Terraform block
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,29 +7,35 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region = local.aws_region
+}
 
 terraform {
   backend "s3" {
-    bucket         = "development-del-s8-tf-state"
-    key            = "aws-secret-manager/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "development-del-s8-tf-state-lock"
-    encrypt        = true
+    bucket         = "danie-s3-bucket"
+    key            = "dev-secret_manager/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "danie-s3-bucket-lock"
   }
 }
+ locals  {
+    aws_region      = "us-east-2"
+    secret_names    = [format("%s-%s-DB_USERNAME", local.tags["environment"], local.tags["project"]) , format("%s-%s-DB_PASSWORD", local.tags["environment"], local.tags["project"])]
+    tags            =  {
+    #Name            =  each.key 
+    owner           = "danniella kitio"
+    teams           = "DevOps"
+    environment     = "dev"
+    project         = "doityourself"
+    create_by       = "danniella"
+    cloud_provider  = "aws"
+    }
+}
 
-module "aws-secret-manager" {
-  source     = "../../../modules/aws-secret-manager"
-  aws_region = "us-east-1"
-  secret_names = [
-    "DB_USERNAME",
-    "DB_PASSWORD"
-  ]
-  tags = {
-    "owner"          = "EK TECH SOFTWARE SOLUTION"
-    "environment"    = "development"
-    "project"        = "alpha"
-    "create_by"      = "Terraform"
-    "cloud_provider" = "aws"
-  }
+module "secret-manager" {
+    source        = "../../../modules/secret-manager"
+    aws_region      = local.aws_region   
+    secret_names    = local.secret_names 
+    tags            = local.tags         
 }
