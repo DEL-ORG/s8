9c9f76f456ca43f9170e94089e5bf8da2fb79366
danniella97 <123521893+danniella97@users.noreply.github.com>
2025-01-03 01:26:58 -0500

    Update main.tf

--- terraform/modules/aws-secret-manager/main.tf
+++ terraform/modules/aws-secret-manager/main.tf
@@ -1,5 +1,4 @@
-resource "aws_secretsmanager_secret" "secrets" {
-  for_each = toset(var.secret_names)
-  name     = each.key
-  tags     = var.tags
+resource "aws_secretsmanager_secret" "my_db_username" {
+   for_each = toset(var.secret_names)
+   name        =  each.key
 }
